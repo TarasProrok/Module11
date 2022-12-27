7a5b8e22aec63bca72a6cc37f2a656009751c30f
TarasProrok <170684@ukr.net>
2022-12-27 15:10:45 +0200

    Added Pushed message

--- OddNames.java
+++ OddNames.java
@@ -1,2 +1,10 @@
 public class OddNames {
+    static void commit() {
+        System.out.println("Initial commit done!");
+        System.out.println("Pushing done!");
+    }
+
+    public static void main(String[] args) {
+        commit();
+    }
 }
